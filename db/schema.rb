d956c5206e260401cf05be4ae7eede6ad1b9ea9f
irvin373 <irvin373@gmail.com>
2016-04-02 22:41:16 -0400

    sqlite3 to mariadb

--- db/schema.rb
+++ db/schema.rb
@@ -13,37 +13,29 @@
 
 ActiveRecord::Schema.define(version: 20160307234737) do
 
-  create_table "books", force: :cascade do |t|
-    t.string   "name"
-    t.string   "autor"
-    t.date     "realise"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "charlas", force: :cascade do |t|
-    t.string   "expositor"
-    t.text     "resumen"
+    t.string   "expositor",           limit: 255
+    t.text     "resumen",             limit: 65535
     t.datetime "hora"
-    t.string   "tema"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
+    t.string   "tema",                limit: 255
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+    t.string   "avatar_file_name",    limit: 255
+    t.string   "avatar_content_type", limit: 255
+    t.integer  "avatar_file_size",    limit: 4
     t.datetime "avatar_updated_at"
-    t.string   "horas"
-    t.string   "minutos"
+    t.string   "horas",               limit: 255
+    t.string   "minutos",             limit: 255
   end
 
   create_table "distros", force: :cascade do |t|
-    t.string   "nombre"
-    t.string   "url"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
+    t.string   "nombre",              limit: 255
+    t.string   "url",                 limit: 255
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "avatar_file_name",    limit: 255
+    t.string   "avatar_content_type", limit: 255
+    t.integer  "avatar_file_size",    limit: 4
     t.datetime "avatar_updated_at"
   end
 
@@ -53,42 +45,44 @@ ActiveRecord::Schema.define(version: 20160307234737) do
   end
 
   create_table "imagens", force: :cascade do |t|
-    t.integer  "distro_id"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
+    t.integer  "distro_id",           limit: 4
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "avatar_file_name",    limit: 255
+    t.string   "avatar_content_type", limit: 255
+    t.integer  "avatar_file_size",    limit: 4
     t.datetime "avatar_updated_at"
   end
 
-  add_index "imagens", ["distro_id"], name: "index_imagens_on_distro_id"
+  add_index "imagens", ["distro_id"], name: "index_imagens_on_distro_id", using: :btree
 
   create_table "requerimientos", force: :cascade do |t|
-    t.string   "requiere"
-    t.integer  "distro_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "requiere",   limit: 255
+    t.integer  "distro_id",  limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
-  add_index "requerimientos", ["distro_id"], name: "index_requerimientos_on_distro_id"
+  add_index "requerimientos", ["distro_id"], name: "index_requerimientos_on_distro_id", using: :btree
 
   create_table "usuarios", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "usuarios", ["email"], name: "index_usuarios_on_email", unique: true
-  add_index "usuarios", ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true
+  add_index "usuarios", ["email"], name: "index_usuarios_on_email", unique: true, using: :btree
+  add_index "usuarios", ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "imagens", "distros"
+  add_foreign_key "requerimientos", "distros"
 end
